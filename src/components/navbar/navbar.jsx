24f8075c3cffcaccefc8552411a14b27f288e12d
unknown <j0ose3x7@gmail.com>
2023-09-08 23:01:29 -0400

    First commit and finished initial setup for navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,23 @@
+import styles from "./navbar.module.css";
+import logo from "../../assets/logo.png";
+import contactImg from '../../assets/contact.png';
+import {Link} from "react-scroll";
+
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <img src={logo} alt="logo" className={`${styles.logo}`} />
+      <div className={styles.desktopMenu}>
+        <Link className={styles.desktopMenuListItem}>Home</Link>
+        <Link className={styles.desktopMenuListItem}>About</Link>
+        <Link className={styles.desktopMenuListItem}>Portfolio</Link>
+      </div>
+      <button className={styles.desktopMenuBtn}>
+        <img src={contactImg} alt="" className={styles.desktopMenuImg} /> Contact Me
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
